0474c32fff4b0a8ead516ce1a77c3371586d48e7
Thariq <thariqskamal@gmail.com>
2015-10-20 16:09:46 +0300

    Ver 1

---
+++ www/js/index.js
@@ -0,0 +1,63 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+    	app.receivedEvent('deviceready');
+    	var networkState = checkConnection();
+    	   /* load local files if there is not network connection */
+    	                  if (networkState == Connection.NONE) {
+    	                   navigator.notification.alert('CMS app requires an internet connection');
+    	                  } else {
+    	                	  window.location="http://172.16.114.12:8000/P2B_CMS";
+    	                  }    	
+    	
+    	
+        //app.receivedEvent('deviceready');
+        //window.location="http://172.16.114.12:8000/P2B_CMS";
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+    
+    
+};
+
+function checkConnection() {
+    var networkState = navigator.network.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.NONE]     = 'No network connection';
+  
+    return networkState;
+  
+}
+
+app.initialize();
